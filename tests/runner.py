6a60dde703367d6f1fdf42c46a0839151d89454e
nikita.prikazchikov <nprikazchikov@mirantis.com>
2013-05-23 17:44:06 +0400

    Add initial packages

---
+++ tests/runner.py
@@ -0,0 +1,14 @@
+import os
+import sys
+
+import nose
+
+from engine.poteen.utils.plugin.PoteenRunnerPlugin import PoteenRunnerPlugin
+
+
+if __name__ == '__main__':
+    nose.main(
+        argv=[os.path.dirname(os.path.abspath(__file__))] + sys.argv[1:]
+             + ["--with-poteenrunnerplugin"],
+        addplugins=[PoteenRunnerPlugin()]
+    )
