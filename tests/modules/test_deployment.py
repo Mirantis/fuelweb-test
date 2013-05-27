c30f16fd77e4dee42a7e2c2f443f483f733b9893
nikita.prikazchikov <nprikazchikov@mirantis.com>
2013-05-27 11:47:29 +0400

    fixed refactoring error

--- tests/modules/test_deployment.py
+++ tests/modules/test_deployment.py
@@ -13,9 +13,9 @@ class Test_Deployment(TestCasePoteen):
     def setUpClass(cls):
         super(Test_Deployment, cls).setUpClass()
         PoteenLogger.add_test_suite("Cluster deployment")
-        # ContextHolder.set_browser("firefox")
-        # ContextHolder.set_do_screenshot(False)
-        # ContextHolder.set_url("http://fuelweb.vm.mirantis.net:8000/")
+        ContextHolder.set_browser("firefox")
+        ContextHolder.set_do_screenshot(False)
+        ContextHolder.set_url("http://fuelweb.vm.mirantis.net:8000/")
 
     @attr(env=["fakeui"], set=["smoke", "regreaasion", "full"])
     def test_deploy_no_ha_1_controller_1_compute(self):
