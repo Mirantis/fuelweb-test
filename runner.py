2017476bae1bc045c92a5d5c875c6530e1d7bc5c
Nikita Prikazchikov <nprikazchikov@mirantis.com>
2013-06-17 11:32:00 +0400

    Setup valid logging through test

--- runner.py
+++ runner.py
@@ -2,13 +2,20 @@ import os
 import sys
 
 import nose
+from engine.poteen.contextHolder import ContextHolder
+import logging
+
+ContextHolder.LOG_LEVEL = logging.DEBUG
+
+selenium_logger = logging.getLogger(
+    'selenium.webdriver.remote.remote_connection'
+)
+selenium_logger.setLevel(logging.WARNING)
 
 from engine.poteen.utils.plugin.poteenRunnerPlugin import PoteenRunnerPlugin
-
-
 if __name__ == '__main__':
     nose.main(
-        argv=[os.path.dirname(os.path.abspath(__file__))] + sys.argv[1:]
-             + ["--with-poteenrunnerplugin"],
+        argv=[os.path.dirname(os.path.abspath(__file__))] + sys.argv[1:] +
+        ["--with-poteenrunnerplugin"],
         addplugins=[PoteenRunnerPlugin()]
     )
--- tests/testdata/cluster.py
+++ tests/testdata/cluster.py
@@ -5,3 +5,10 @@ class TD_Cluster(object):
         super(TD_Cluster, self).__init__()
         self.name = ""
         self.version = ""
+
+    def __repr__(self):
+        return str(dict(
+            class_name="TD_Cluster",
+            name=self.name,
+            version=self.version
+        ))
