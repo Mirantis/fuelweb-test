668583ef9e0a73ebab812b85436c41205c167df7
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-07-16 20:35:43 +0400

    Add methods to work with ip ranges

--- tests/components/functionality/cluster/generic/ip_range_row.py
+++ tests/components/functionality/cluster/generic/ip_range_row.py
@@ -2,6 +2,7 @@ from engine.poteen.basePage import BasePage
 from engine.poteen.elements.basic.button import Button
 from engine.poteen.elements.basic.input import Input
 
+from engine.poteen.log.resultList import ResultList
 
 class IpRangeRow(BasePage):
     def __init__(self, parent=None):
@@ -22,3 +23,32 @@ class IpRangeRow(BasePage):
             element_name="Ip range delete")
 
         BasePage.__init__(self, parent)
+
+    def set_start_ip(self, args):
+        return self.ip_range_start.set_value(args)
+
+    def is_invalid(self):
+        return self.ip_range_start.verify_attribute("class", "error")
+
+    def is_correct(self):
+        return self.ip_range_start.verify_attribute("class", "")
+
+    def set_start_ip_verify_error(self, args):
+        rl = ResultList("Set ip range start with '{args}' "
+                        "and verify error".format(args=args)) \
+            .push(self.set_start_ip(args)) \
+            .push(self.is_invalid())
+        return rl
+
+    def set_start_ip_correct(self, args):
+        rl = ResultList("Set ip range start with '{args}' and verify "
+                        "error does not exist".format(args=args)) \
+            .push(self.set_start_ip(args)) \
+            .push(self.is_correct())
+        return rl
+
+    def set_start_ip_and_verify(self, args, value):
+        if value:
+            return self.set_start_ip_correct(args)
+        else:
+            return self.set_start_ip_verify_error(args)
