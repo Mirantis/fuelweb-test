d22c2bc6050f046765f7416ac564f8821ff967c9
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-08-22 19:22:39 +0400

    Add credentials parser plugin in runner.py

--- runner.py
+++ runner.py
@@ -4,6 +4,8 @@ import sys
 import nose
 from engine.poteen.contextHolder import ContextHolder
 import logging
+from tests.components.helpers.RHCredentialsParserPlugin \
+    import CredentialsParserPlugin
 
 ContextHolder.LOG_LEVEL = logging.DEBUG
 
@@ -16,6 +18,6 @@ from engine.poteen.utils.plugin.poteenRunnerPlugin import PoteenRunnerPlugin
 if __name__ == '__main__':
     nose.main(
         argv=[os.path.dirname(os.path.abspath(__file__))] + sys.argv[1:] +
-        ["--with-poteenrunnerplugin"],
-        addplugins=[PoteenRunnerPlugin()]
+        ["--with-poteenrunnerplugin"]+["--with-credentialsparserplugin"],
+        addplugins=[PoteenRunnerPlugin(), CredentialsParserPlugin()]
     )
