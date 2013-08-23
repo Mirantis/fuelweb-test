b5b9a1dcd1b2b9204979362502b559dc97249aa7
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-08-23 14:40:33 +0400

    Add elements of openstack settings page

--- tests/components/functionality/cluster/openstack_settings/view.py
+++ tests/components/functionality/cluster/openstack_settings/view.py
@@ -1,9 +1,22 @@
+from engine.poteen.elements.basic.button import Button
 from engine.poteen.elements.basic.input import Input
 from ....generic.abstractView import AbstractView
 
 
 class OpenstackSettingsView(AbstractView):
     def __init__(self, parent=None):
+        self.save_settings = Button(
+            xpath=".//button[contains(@class, 'btn-apply-changes')]",
+            element_name="Save Settings")
+
+        self.cancel_changes = Button(
+            xpath=".//button[contains(@class, 'btn-revert-changes')]",
+            element_name="Cancel Changes")
+
+        self.load_defaults = Button(
+            xpath=".//button[contains(@class, 'btn btn-load-defaults')]",
+            element_name="Load Defaults")
+
         self.parameter_input = Input(
             xpath=".//label[contains(@class,'parameter-box') and "
                   "div[contains(@class,'parameter-name')]='{name}']//"
@@ -22,6 +35,20 @@ class OpenstackSettingsView(AbstractView):
                   "input[@type='checkbox']",
             element_name="Parameter {name}")
 
+        self.show_password = Button(
+            xpath=".//span[@class='add-on' "
+                  "and i[contains(@class, 'icon-eye')]]",
+            element_name="Show password button")
+
+        self.show_password_on = Button(
+            xpath=".//i[@class ='icon-eye' and @style='display: inline;']",
+            element_name="Show password button")
+
+        self.show_password_off = Button(
+            xpath=".//i[@class ='icon-eye-off hide' "
+                  "and @style='display: inline;']",
+            element_name="Show password button")
+
         AbstractView.__init__(self, parent)
 
     def set_parameter_input(self, name, value):
@@ -32,3 +59,6 @@ class OpenstackSettingsView(AbstractView):
 
     def set_parameter_checkbox(self, name, value):
         return self.parameter_checkbox.find(name=name).set_value(value)
+
+    def verify_parameter_input(self, name, value):
+        return self.parameter_input.find(name=name).verify_value(value)
