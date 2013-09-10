dd13f988248ca9c8e8c7e0cd0c33b78f4c51b5e8
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-09-10 14:52:20 +0400

    Add create environment method

--- tests/components/functionality/cluster/dialogs/createEnvironmentDialog.py
+++ tests/components/functionality/cluster/dialogs/createEnvironmentDialog.py
@@ -9,6 +9,9 @@ from engine.poteen.utils.storage import Storage
 from .....components.generic.abstractDialog import AbstractDialog
 from .....testdata.cluster import TD_Cluster
 from .....components.elements.Radio import Radio
+from .environmentDeploymentModeDialog import EnvironmentDeploymentModeDialog
+from tests.components.functionality.cluster.openstack_settings.view \
+    import OpenstackSettingsView
 
 
 class CreateEnvironmentDialog(AbstractDialog):
@@ -26,7 +29,7 @@ class CreateEnvironmentDialog(AbstractDialog):
             xpath=".//select[@name='release']", element_name="Version"
         )
         self.downloadType = Radio(
-            xpath=".//div[contains(@class, 'custom-tumbler') "
+            xpath="//div[contains(@class, 'custom-tumbler') "
                   "and input[@type='radio' and @value='{type}']]",
             element_name="Download type [{type}]")
 
@@ -54,10 +57,15 @@ class CreateEnvironmentDialog(AbstractDialog):
             xpath=".//div[@class='release-description help-block']",
             element_name="Release description"
         )
+        self.message4thStep = HtmlElement(
+            xpath=".//div[contains(text(), 'Configuration is finished and "
+                  "now you can create your cluster!')]",
+            element_name="Message: ready to create environment"
+        )
 
         AbstractDialog.__init__(self)
 
-    def populate(self, name, version, submit=False):
+    def populate(self, name, version, clickNext=False):
         environment = Storage.get_current(TD_Cluster.NAME)
         environment.name = name
         environment.version = version
@@ -67,10 +75,10 @@ class CreateEnvironmentDialog(AbstractDialog):
             .push(WaitBot().wait_loading()) \
             .push(self.version.set_value(version)) \
             .push(self.name.click())
-        if submit:
+        if clickNext:
             WaitBot().wait_loading()
             WaitBot().wait_for_stop_resizing(By.XPATH, self.XPATH_DIALOG)
-            rl.push(self.create())
+            rl.push(self.clickNext())
             WaitBot().wait_loading()
         return rl
 
@@ -84,24 +92,54 @@ class CreateEnvironmentDialog(AbstractDialog):
         return self.downloadType.find(type=value).set_value("on")
 
     def populateRHOS(self, name, version, downloadMode, username, password,
-                     serverHostName, activationKey, submit=False):
+                     serverHostName, activationKey, clickNext=False):
         rl = ResultList("Populate new environment dialog with RHOS") \
             .push(self.populate(name, version))
         WaitBot().wait_loading()
         WaitBot().wait_for_stop_resizing(By.XPATH, self.XPATH_DIALOG)
-        if VerifyBot().is_element_displayed(self.downloadType.find(
-                type=downloadMode)):
+        if VerifyBot().verify_visibility(self.instruction.get_element(),
+                                         True, "instruction").i_passed():
             rl.push(self.select_download_mode(downloadMode))
-            rl.push(VerifyBot().verify_visibility(
-                    self.instruction.get_element(), True, "instruction"))
             rl.push(self.username.set_value(username))
             rl.push(self.password.set_value(password))
             if downloadMode == 'rhn':
                 rl.push(self.serverHostname.set_value(serverHostName))
                 rl.push(self.activationKey.set_value(activationKey))
-        if submit:
+        if clickNext:
             WaitBot().wait_loading()
             WaitBot().wait_for_stop_resizing(By.XPATH, self.XPATH_DIALOG)
-            rl.push(self.create())
+            rl.push(self.clickNext())
             WaitBot().wait_loading()
+        return rl
+
+    def createEnvironment(self, name, version, deploymentMode, computeType):
+        rl = ResultList("Create new environment") \
+            .push(self.populate(name, version, True))
+        rl.push(EnvironmentDeploymentModeDialog().populate(
+            deploymentMode, True))
+        rl.push(OpenstackSettingsView().set_parameter_radio(
+            computeType, "on"))
+        rl.push(self.clickNext())
+        rl.push(VerifyBot().verify_visibility(
+            self.message4thStep.get_element(), True,
+            "Message: ready to create environment"))
+        rl.push(self.create())
+        return rl
+
+    def createEnvironmentRHOS(self, name, version, downloadMode, username,
+                              password, serverHostName, activationKey,
+                              deploymentMode, computeType):
+        rl = ResultList("Create new environment") \
+            .push(self.populateRHOS(name, version, downloadMode, username,
+                                    password, serverHostName, activationKey,
+                                    True))
+        rl.push(EnvironmentDeploymentModeDialog().populate(
+            deploymentMode, True))
+        rl.push(OpenstackSettingsView().set_parameter_radio(
+            computeType, "on"))
+        rl.push(self.clickNext())
+        rl.push(VerifyBot().verify_visibility(
+            self.message4thStep.get_element(), True,
+            "Message: ready to create environment"))
+        rl.push(self.create())
         return rl
