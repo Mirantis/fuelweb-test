a2b53a9b56f423ae19853f8535024db80a35e30d
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-09-13 18:40:07 +0400

    Add buttons to deploy changes dialog

--- tests/components/functionality/cluster/dialogs/deployChangesDialog.py
+++ tests/components/functionality/cluster/dialogs/deployChangesDialog.py
@@ -1,6 +1,17 @@
 from .....components.generic.abstractDialog import AbstractDialog
+from engine.poteen.elements.basic.button import Button
+from engine.poteen.elements.basic.htmlElement import HtmlElement
 
 
 class DeployChangesDialog(AbstractDialog):
     def __init__(self):
         AbstractDialog.__init__(self)
+        self.disabled_deploy_btn = Button(
+            xpath=".//button["
+                  "contains(@class,'start-deployment-btn disabled')]",
+            element_name="Disabled deploy button"
+        )
+        self.alert_message = HtmlElement(
+            xpath=".//div[@class='alert alert-error']",
+            element_name="Alert error message"
+        )
