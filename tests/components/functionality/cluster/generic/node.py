6602c707caf7d8598f2c74a0242ab89d8b35dd3d
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-09-13 15:50:19 +0400

    Add checkboxes

--- tests/components/functionality/cluster/generic/node.py
+++ tests/components/functionality/cluster/generic/node.py
@@ -1,15 +1,26 @@
+
 from engine.poteen.elements.basic.htmlElement import HtmlElement
 from engine.poteen.log.result import Result
 from ....generic.abstractView import AbstractView
+from tests.components.elements.Checkbox import Checkbox
 
 
 class Node(AbstractView):
     def __init__(self, parent=None):
         self.name = HtmlElement(
-            xpath=".//div[@class='node-name']", element_name="Cluster name")
+            xpath=".//div[@class='node-name']//p[@class='node-renameable']",
+            element_name="Cluster name"
+        )
 
-        self._select = HtmlElement(
-            xpath=".//div[@class='node-select']", element_name="Select tick")
+        self.role = HtmlElement(
+            xpath=".//div[@class='roles']//li",
+            element_name="Cluster role"
+        )
+
+        self._checkbox = Checkbox(
+            xpath=".//div[@class='node-checkbox']"
+                  "//div[@class='custom-tumbler']/input",
+            element_name="Checkbox")
 
         self.status = HtmlElement(
             xpath=".//div[@class='node-status']", element_name="Status")
@@ -25,14 +36,17 @@ class Node(AbstractView):
     def get_status(self):
         return self.status.get_value().strip()
 
-    def is_selected(self):
-        return self._select.get_element().is_displayed()
+    def verify_checkbox(self, expectedValue):
+        return self._checkbox.verify_value(expectedValue)
+
+    def set_checkbox(self, value):
+        return self._checkbox.set_value(value)
 
     def select(self):
-        if self.is_selected():
+        if self.verify_checkbox("on").i_passed():
             return Result("Node is already selected")
         else:
-            return self.name.click()
+            return self.set_checkbox("on")
 
     def click_hardware(self):
         return self.hardware.click()
