fa495e2b4bb3c90e3476bf81f2470574dbd294d4
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-09-18 12:03:09 +0400

    Fix xpath

--- tests/components/functionality/cluster/nodes/listView.py
+++ tests/components/functionality/cluster/nodes/listView.py
@@ -36,7 +36,7 @@ class Cluster_Nodes_ListView(AbstractView):
         return nodes_names
 
     def get_nodes(self):
-        return self._get_nodes(".//div[contains(@class,'span2 node-box')]")
+        return self._get_nodes(".//div[contains(@class,'node-box')]")
 
     def get_nodes_by_status(self, status):
         return self._get_nodes(
