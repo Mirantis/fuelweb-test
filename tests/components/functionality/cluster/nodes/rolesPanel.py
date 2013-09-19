af75f3ebece8b66dc9db26cd556a1338eaf47e16
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-09-19 17:35:20 +0400

    Fix xpath to find elements by role

--- tests/components/functionality/cluster/nodes/rolesPanel.py
+++ tests/components/functionality/cluster/nodes/rolesPanel.py
@@ -11,32 +11,12 @@ class RolesPanel(AbstractView):
             xpath="//input[@type='checkbox' and @value='{role}']",
             element_name="Checkbox '{role}'"
         )
-        self.checkbox_controller = Checkbox(
-            xpath=".//input[@type='checkbox' and @value='controller']",
-            element_name="Checkbox controller"
-        )
-        self.checkbox_compute = Checkbox(
-            xpath=".//input[@type='checkbox' and @value='compute']",
-            element_name="Checkbox compute"
-        )
-        self.checkbox_cinder = Checkbox(
-            xpath=".//input[@type='checkbox' and @value='cinder']",
-            element_name="Checkbox cinder"
-        )
         self.assign = Button(
             xpath=".//button[@class='btn btn-success btn-assign']",
             element_name="Assign button"
         )
-        self.conflict_controller = HtmlElement(
-            xpath="role-conflict controller",
-            element_name="Conflict controller"
-        )
-        self.conflict_compute = HtmlElement(
-            xpath="role-conflict compute",
-            element_name="Conflict compute"
-        )
-        self.conflict_cinder = HtmlElement(
-            xpath="role-conflict cinder",
-            element_name="Conflict cinder"
+        self.conflict_role = HtmlElement(
+            xpath=".//div[@class='role-conflict '{role}'']",
+            element_name="Conflict '{role}'"
         )
         AbstractView.__init__(self, parent)
