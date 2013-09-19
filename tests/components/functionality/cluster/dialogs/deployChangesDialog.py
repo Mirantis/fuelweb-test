ca8bf7fc1c1aef8de2234630e1d6b5c63a99c098
Kseniya Cherchenko <kcherchenko@mirantis.com>
2013-09-19 17:34:07 +0400

    Delete element disabled deploy button

--- tests/components/functionality/cluster/dialogs/deployChangesDialog.py
+++ tests/components/functionality/cluster/dialogs/deployChangesDialog.py
@@ -6,11 +6,7 @@ from engine.poteen.elements.basic.htmlElement import HtmlElement
 class DeployChangesDialog(AbstractDialog):
     def __init__(self):
         AbstractDialog.__init__(self)
-        self.disabled_deploy_btn = Button(
-            xpath=".//button["
-                  "contains(@class,'start-deployment-btn disabled')]",
-            element_name="Disabled deploy button"
-        )
+
         self.alert_message = HtmlElement(
             xpath=".//div[@class='alert alert-error']",
             element_name="Alert error message"
