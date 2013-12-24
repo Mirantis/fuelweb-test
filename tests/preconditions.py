aee4d883d848201d072666626730acf23a1b9669
nfedotov <nfedotov@mirantis.com>
2013-12-24 11:52:03 +0400

    precoditions module
    It contains precoditions actions

---
+++ tests/preconditions.py
@@ -0,0 +1,19 @@
+from pageobjects.environments import Environments, Wizard
+from settings import *
+from tests.base import BaseTestCase
+
+
+class Environment:
+
+    @staticmethod
+    def simple_flat(name=OPENSTACK_CENTOS,
+                    release=OPENSTACK_RELEASE_CENTOS):
+        BaseTestCase.get_home()
+        Environments().create_cluster_box.click()
+        with Wizard() as w:
+            w.name.send_keys(name)
+            w.release.select_by_visible_text(release)
+            for i in range(6):
+                w.next.click()
+            w.create.click()
+            w.wait_until_exists()
